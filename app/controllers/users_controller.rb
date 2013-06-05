abf07c702fc21d2e8d92607918f81daf49223dbf
wikimo <gwikimo@gmail.com>
2013-06-05 22:39:38 +0800

    update user topic display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
   	@user =  User.find params[:id]
+
+    @topics =  Topic.find(:all, :conditions =>["user_id = ?",@user.id])
   end
 
   def new
