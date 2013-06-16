20aadc04c36497c20e746b0a8364a9e9d9ff60c5
chen zhong <cedric_chenzhong@163.com>
2013-06-16 11:25:17 +0800

    category

--- app/controllers/cpanel/categories_controller.rb
+++ app/controllers/cpanel/categories_controller.rb
@@ -3,7 +3,32 @@ class Cpanel::CategoriesController < Cpanel::ApplicationController
   def index
   	
   	@categories = Category.all
+  	@category = Category.new
+  end
 
+
+  def create
+  	@category = Category.create(:name => params[:category][:name], :description => params[:category][:description])
+
+  	if @category.save
+  		redirect_to cpanel_categories_path,:notice  => 'create_group_success'
+  	else
+
+  	end
+  end
+
+
+  def update
+  	
+  end
+
+
+  def show
+  	
+  end
+
+  def destroy
+  	
   end
 
 end
