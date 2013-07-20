acc6239f8be5172e8151ffd5b905d013f5799131
wikimo <gwikimo@gmail.com>
2013-07-20 16:11:02 +0800

    fix group admin

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,6 @@ class TopicsController < ApplicationController
 		if current_user
 			@gu = GroupUser.find_by_group_id_and_user_id(@group.id, current_user.id)
 
-			p @gu
 		end
 
 		@members = @group.members
@@ -18,7 +17,7 @@ class TopicsController < ApplicationController
 		@creater = @group.creater
 
 		@managers = @group.managers
-		puts @managers.size
+	
 	end
 
 
