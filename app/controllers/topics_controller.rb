a296ad19b87c767bc339026aed1172b7e9de9154
chen zhong <cedric_chenzhong@163.com>
2013-08-04 15:30:13 +0800

    update topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -49,6 +49,26 @@ class TopicsController < ApplicationController
 
 	end
 
+	def edit
+
+		@topic = Topic.find(params[:id])
+
+		@group = @topic.group
+	end
+
+	def update
+		@topic = Topic.find(params[:id])
+		params[:topic][:title] = params[:topic][:title].gsub(/\r\n/, "<br/>")
+		params[:topic][:content] = params[:topic][:content].gsub(/\r\n/,"<br/>")
+
+		if @topic.update_attributes(params[:topic])
+			update_photos(params[:photo_id],@topic)
+			redirect_to @topic, :notice => t(:update_success)
+		else
+			#error
+		end
+	end
+
 	def show
 		@topic = Topic.find params[:id]
 
