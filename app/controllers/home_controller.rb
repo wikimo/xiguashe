b51beecaca55e668735eef8e5392dd6e93222562
wikimo <gwikimo@gmail.com>
2013-10-29 23:25:28 +0800

    update root routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,6 @@
 class HomeController < ApplicationController
 	before_filter :login_redirect, :except => [:about,:contact,:links]
 
-	def index
-		#@topics =  Topic.find(:all,:limit => 10,:order => 'id desc')
-		# @recent_topics =  Topic.recent_topics
-		# recent_tids =  @recent_topics.map(&:id)
-
-		# @like_topics = Topic.like_topics(recent_tids)
-		redirect_to recommend_topic_path
-		
-	end
-
 	def about
 		
 	end
