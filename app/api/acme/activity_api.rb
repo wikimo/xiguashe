8d950e54211958c8fc3d412e16246b0d848a3e64
chen zhong <cedric_chenzhong@163.com>
2014-01-02 21:01:14 +0800

    update api

--- app/api/acme/activity_api.rb
+++ app/api/acme/activity_api.rb
@@ -1,15 +1,23 @@
 module Acme
   class ActivityAPI < Grape::API
+
     format :json
 
     resource :activities do
       params do
         requires :page, type: String, desc: "page"
         requires :per_page, type: String, desc: "per_page"
+        requires :id, type: String, desc: "id"
       end
+
       get :index do
         @activities = Activity.status_can_use.order_by_created_at_desc.paginate(page:params[:page], per_page: params[:per_page])
       end
+
+      get :show do
+        @activity = Activity.find params[:id]
+      end
+
     end
   end
 end
