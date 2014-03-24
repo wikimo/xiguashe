5cadd4969ff7a60cd909359294edafaee4a6f0db
wikimo <gwikimo@gmail.com>
2014-03-24 20:44:51 +0800

    add img to topic

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,8 +1,9 @@
 module TopicsHelper
 
   def topic_photos_helper(topic)
-    unless topic.photos.empty?
-      image_tag topic.photos.first.path.url(:original), alt: topic.title
+    unless topic.img.nil? || topic.img.url.include?('blank')
+      #image_tag topic.photos.first.path.url(:original), alt: topic.title
+      image_tag topic.img.url, :alt => topic.title
     end
   end
 end
