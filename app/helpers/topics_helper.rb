bb35324c933c4504829fc1aa67df87211650a4ba
wikimo <gwikimo@gmail.com>
2014-03-24 15:51:10 +0800

    for push

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,6 +3,7 @@ module TopicsHelper
   def topic_photos_helper(topic)
     unless topic.photos.empty?
       image_tag topic.photos.first.pic.url(:original), alt: topic.title
+      
     end
   end
 end
