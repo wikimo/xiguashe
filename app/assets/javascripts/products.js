bf30ba648f5d21340319173534f5dbbdfe2d2b1d
chen zhong <cedric_chenzhong@163.com>
2014-03-26 22:43:53 +0800

    product update

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -7,7 +7,57 @@ $(function(){
     $(this).parent().remove();
 
   });
-  
+
+  $("#photo_").change(function(){
+    var data = new FormData();
+
+    var arr = [];
+    $.each($('#photo_')[0].files, function(i, file) {
+
+      if (file.type.indexOf("image") != 0) {
+        $('#error').prepend(file.name + ':不是图片')
+        return false;
+      }
+
+      if (file.size >= 5120000) {
+        $('#error').prepend( file.name + ':大于5M');
+        return false;
+      }
+
+      data.append('photo', file);
+      data.append('auth_token', '#{cookies[:auth_token]}');
+
+      $(".loading").show();
+
+      $.ajax({
+        type: 'post',
+        url: '/photos',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        contentType: false,       
+        processData: false,
+        success: function(obj) {
+
+          if (obj['msg'] == 'ok') {
+            $('.good-img-list').append('<li id="new-photo-' + obj['id'] + '"></li>');
+            $('#new-photo-' + obj['id']).prepend('<input type="hidden" name="photo_id[]" value="' + obj['id'] + '" />')
+            $('#new-photo-' + obj['id']).prepend('<a  class ="glyphicon glyphicon-remove-circle" id="del-' + obj['id'] + '" href="javascript:del_photo(' + obj['id'] + ')"></a>');
+            $('#new-photo-' + obj['id']).prepend('<input type="radio" name="radio_img" value=' + obj['photo'] +' /> 选择封面')
+            $('#new-photo-' + obj['id']).prepend('<img src=' + obj['photo'] + ' alt="" /><br/>');
+          }
+          
+          $(".loading").hide();
+        },
+        error: function() {
+          $('#error').prepend('<li>出错</li>');
+          $(".loading").hide();
+        }
+      });
+    });
+    $('#photo_').val('');
+  }); 
+
   $('#get-product').click(function(){
 
      var link = $('input[name="link"]').val();
@@ -28,14 +78,16 @@ $(function(){
        processData: false,
        dataType: 'json',
        success: function(obj) {
-         $('.alert').show();
          if ( obj['text'] == 0 ) {
+           $('.alert').show();
            $('.alert > strong').html(obj['msg']);
          }
          else if ( obj['text'] == 1 ) {
+           $('.alert').show();
            $('.alert > strong').html(obj['msg']);
          }
          else if ( obj['text'] == 2 ) {
+           $('.alert').show();
            $('.alert > strong').html(obj['msg']);
 
            $.ajax({
@@ -47,7 +99,6 @@ $(function(){
              success: function(obj) {
 
                $('#exist-product').prepend('点击查看：<a href="/products/' + obj['product']['id'] + '">[' + obj['product']['title'] + ']</a>');
-               //$('#exist-product').prepend('<a href="/products/' + obj['product']['id'] + '"><img src="' + obj['product']['img'] + '"/></a>');
 
              }
            });
@@ -83,7 +134,20 @@ $(function(){
     }, 3000);
   }
 
-
 });
+
+
+function del_photo(id) {
+  $.ajax({
+    type: 'DELETE',
+    url: '/photos/' + id,
+    success: function(obj) {
+      if (obj['msg'] == 'ok') {
+        $('#del-'+id).parent().remove();
+      }
+    }
+  });
+}
+
         
         
--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,68 +1,7 @@
 $(function(){
-  $("#photo_").change(function(){
-    var data = new FormData();
 
-    var arr = [];
-    $.each($('#photo_')[0].files, function(i, file) {
-
-      if (file.type.indexOf("image") != 0) {
-        $('#error').prepend(file.name + ':不是图片')
-        return false;
-      }
-
-      if (file.size >= 5120000) {
-        $('#error').prepend( file.name + ':大于5M');
-        return false;
-      }
-
-      data.append('photo', file);
-      data.append('auth_token', '#{cookies[:auth_token]}');
-
-      $(".loading").show();
-
-      $.ajax({
-        type: 'post',
-        url: '/photos',
-        data: data,
-        cache: false,
-        dataType: 'json',
-        contentType: false,       
-        processData: false,
-        success: function(obj) {
-
-          if (obj['msg'] == 'ok') {
-            $('.good-img-list').append('<li id="new-photo-' + obj['id'] + '"></li>');
-            $('#new-photo-' + obj['id']).prepend('<input type="hidden" name="photo_id[]" value="' + obj['id'] + '" />')
-            $('#new-photo-' + obj['id']).prepend('<a  class ="glyphicon glyphicon-remove-circle" id="del-' + obj['id'] + '" href="javascript:del_photo(' + obj['id'] + ')"></a>');
-            $('#new-photo-' + obj['id']).prepend('<input type="radio" name="radio_img" value=' + obj['photo'] +' /> 选择封面')
-            $('#new-photo-' + obj['id']).prepend('<img src=' + obj['photo'] + ' alt="" /><br/>');
-          }
-          
-          $(".loading").hide();
-        },
-        error: function() {
-          $('#error').prepend('<li>出错</li>');
-          $(".loading").hide();
-        }
-      });
-    });
-    $('#photo_').val('');
-  });
 });
 
-
-function del_photo(id) {
-  $.ajax({
-    type: 'DELETE',
-    url: '/photos/' + id,
-    success: function(obj) {
-      if (obj['msg'] == 'ok') {
-        $('#del-'+id).parent().remove();
-      }
-    }
-  });
-
-}
 
 
 
