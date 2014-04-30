e197b1b20100242c0d342ba056fa1bec23b71ab4
wikimo <gwikimo@gmail.com>
2014-04-30 15:35:50 +0800

    update tbk & weixin share

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,3 +1,4 @@
+# encoding:utf-8
 module PhotosHelper
 
   def photo_choose_helper(photo)
@@ -17,6 +18,15 @@ module PhotosHelper
             else options[:style].to_s
             end
     path.url(style)
+  end
+
+  def convert_product_url(url)
+    if url.include?('taobao') || url.include?('tmall')
+      id = url.split('id=').last
+      return raw "<a data-type='0' biz-itemid='#{id}' data-rd='2' data-style='2'  href='#{url}' class='btn btn-primary' data-tmplid='1111' target='_blank'>去购买</a>"
+    end
+
+    link_to '去购买', url, class: "btn btn-primary",target: '_blank'
   end
 
 end
