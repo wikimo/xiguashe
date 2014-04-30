8a0069b4e3d682e4b88d036e4f096f3ec4e597cf
wikimo <gwikimo@gmail.com>
2014-04-30 15:42:55 +0800

    update tbk

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -20,13 +20,4 @@ module PhotosHelper
     path.url(style)
   end
 
-  def convert_product_url(url)
-    if url.include?('taobao') || url.include?('tmall')
-      id = url.split('id=').last
-      return raw "<a data-type='0' biz-itemid='#{id}' data-rd='2' data-style='2'  href='#{url}' class='btn btn-primary' data-tmplid='1111' target='_blank'>去购买</a>"
-    end
-
-    link_to '去购买', url, class: "btn btn-primary",target: '_blank'
-  end
-
 end
