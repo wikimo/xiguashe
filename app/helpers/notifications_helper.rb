6f6d912afb3290a5898539e9409cca4cc9a4aa96
chen zhong <cedric_chenzhong@163.com>
2014-05-04 21:36:37 +0800

    add new tag in notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,8 +2,11 @@
 module NotificationsHelper
 
   def mention_id_helper(mention_id)
-
     mention_id.nil? ? '评论' : '艾特'
-  
   end
+
+  def is_read_helper(notification, &block)
+    block.call unless notification.try(:is_read?)
+  end
+
 end
